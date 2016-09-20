75232760038fae4f1836ad7dbb9d3d6ba1c60332
Glenn Matthys <glenn@webmind.be>
2016-09-20 13:32:12 +0200

    Remove old discovery script

--- zabbix_md_discover.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo -n '{"data": [';
-sep='';
-for i in `find /sys/block -name md* -printf "%f\n"`; do 
-        echo -n $sep'{ "{#MDNAME}": "'$i'" }';
-        sep=','
-done;
-
-echo -n ']}'
